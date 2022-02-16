227509edd9e6e6765dc793c32217bd7332d4b871
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-16 18:56:43 +0900

    [FEAT] 데이터베이스 h2 설정

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,14 +1,40 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
-
+@Transactional
 class QuerydslApplicationTests {
 
+	@Autowired
+	EntityManager em;
+
 	@Test
+
 	void contextLoads() {
+
+		testEntity testEntity = new testEntity();
+		em.persist(testEntity);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QtestEntity qtest = QtestEntity.testEntity;
+
+		testEntity result = query
+				.selectFrom(qtest)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(testEntity);
+		assertThat(result.getId()).isEqualTo(testEntity.getId());
+
 	}
 
 }

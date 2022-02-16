ebc9da7f3a8588aa2441c99b63f7f97fd726488e
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-16 19:25:11 +0900

    [FEAT] DB 테스트

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,10 +1,10 @@
 package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -13,27 +13,15 @@ import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
 	EntityManager em;
 
 	@Test
-
 	void contextLoads() {
 
-		testEntity testEntity = new testEntity();
-		em.persist(testEntity);
-
-		JPAQueryFactory query = new JPAQueryFactory(em);
-		QtestEntity qtest = QtestEntity.testEntity;
-
-		testEntity result = query
-				.selectFrom(qtest)
-				.fetchOne();
-
-		assertThat(result).isEqualTo(testEntity);
-		assertThat(result.getId()).isEqualTo(testEntity.getId());
 
 	}
 

285f6bcdfe08774fed77a7cedd9a3c6b89b09720
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-17 01:00:28 +0900

    [FEAT] queryDsl 기본문법 2

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,8 @@
 package study.querydsl;
 
 
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
+import study.querydsl.entity.QTeam;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -17,6 +20,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.*;
+import static study.querydsl.entity.QTeam.*;
 
 @SpringBootTest
 @Transactional
@@ -99,6 +103,202 @@ public class QuerydslBasicTest {
         assertThat(findMember.getAge()).isEqualTo(10);
         //then
     }
+
+    @Test
+    public void resultFetchTest() throws Exception {
+        //given
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        Member member1 = queryFactory
+                .selectFrom(QMember.member)
+                .fetchOne();
+
+        Member fetchFirst = queryFactory
+                .selectFrom(QMember.member)
+                .fetchFirst();
+
+        QueryResults<Member> memberQueryResults = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+        memberQueryResults.getTotal();
+        List<Member> content = memberQueryResults.getResults();
+
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+    /**
+     *  회원 정렬 순서
+     *  1. 회원 나이 내림차순
+     *  2. 회원 이름 올림차순
+     *  3단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     * @throws Exception
+     */
+    @Test
+    public void 정렬() throws Exception {
+        em.persist(new Member(null,100));
+        em.persist(new Member("member5",100));
+        em.persist(new Member("member6",100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
+    }
+
+    @Test
+    public void 페이징() throws Exception {
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+    @Test
+    public void 전체페이징() throws Exception {
+
+        QueryResults<Member> QueryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+        assertThat(QueryResults.getTotal()).isEqualTo(4);
+        assertThat(QueryResults.getLimit()).isEqualTo(2);
+        assertThat(QueryResults.getOffset()).isEqualTo(1);
+        assertThat(QueryResults.getResults().size()).isEqualTo(2);
+
+    }
+
+    @Test
+    public void 집합() throws Exception {
+        List<Tuple> findMembers = queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+        Tuple tuple = findMembers.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+    /**
+     * 팀의 이름과 각 팀의 평균 연령을 구해라.
+     * @throws Exception
+     */
+
+    @Test
+    public void 그룹() throws Exception {
+        //given
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+        assertThat(teamA.get(team.name)).isEqualTo("teamB");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(35);
+    }
+
+
+    /**
+     * 팀 A에 소속된 모든 회원
+     * @throws Exception
+     */
+    @Test
+    public void join() throws Exception {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1","member2");
+    }
+
+    /**
+     * 세타 조인
+     * 연관관계 없는 필드로 조인
+     * 모든 필드를 묶어서 검사
+     * 회원 이름이 팀이름과 같은 회원 조회
+     * @throws Exception
+     */
+    @Test
+    public void theta_join() throws Exception {
+        //given
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        //when
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+        //then
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
+
+    /**
+     * 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL : select m,t from Member m left join m.team t on t.name = 'teamA'
+     * @throws Exception
+     */
+    @Test
+    public void 조인_온_filtering() throws Exception {
+        //given
+        List<Tuple> reuslt = queryFactory
+                .select(member, team)
+                .from(member)
+                .join(member.team, team)
+//                .join(member.team, team).on(team.name.eq("teamA"))
+                .where(team.name.eq("teamA")) //필터링  이너일때는 on과 동일
+                .fetch();
+        for (Tuple tuple : reuslt) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+
+
+
+
+
+
+
 
 
 
